1b3765fde9cbfe930549b65e4bbf234a1cf55aba
Pavel <whatisclamp@gmail.com>
2021-08-04 21:51:36 +0300

    Some

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,33 +1,54 @@
-window.addEventListener('DOMContentLoaded', () => { 
+window.addEventListener('DOMContentLoaded', () => {
     //change language
     const langRuBtn = document.querySelector('.header__menu-ru'),
         langEnBtn = document.querySelector('.header__menu-eng');
 
-        langEnBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            langEnBtn.classList.add('header__menu--active', 'animate__animated', 'animate__slideInLeft', 'animate__faster');
-            langRuBtn.classList.remove('header__menu--active', 'animate__animated', 'animate__slideInRight', 'animate__faster');
+    langEnBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        langEnBtn.classList.add('header__menu--active', 'animate__animated', 'animate__slideInLeft', 'animate__faster');
+        langRuBtn.classList.remove('header__menu--active', 'animate__animated', 'animate__slideInRight', 'animate__faster');
 
-        });
+    });
 
-        langRuBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            langRuBtn.classList.add('header__menu--active', 'animate__animated', 'animate__slideInRight', 'animate__faster');
-            langEnBtn.classList.remove('header__menu--active', 'animate__animated', 'animate__slideInLeft', 'animate__faster');
-        })
+    langRuBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        langRuBtn.classList.add('header__menu--active', 'animate__animated', 'animate__slideInRight', 'animate__faster');
+        langEnBtn.classList.remove('header__menu--active', 'animate__animated', 'animate__slideInLeft', 'animate__faster');
+    })
 
-        //change Mega Tabs 
+    //change Mega Tabs 
 
-        const tabWebBtn = document.querySelector('.header__menu-link--web'),
-            tabAdBtn = document.querySelector('.header__menu-link--ad');
+    const tabWebBtn = document.querySelector('.header__menu-link--web'),
+        tabAdBtn = document.querySelector('.header__menu-link--ad'),
+        promoAdBtn = document.getElementById('promo__adv'),
+        promoWebBtn = document.getElementById('promo__web'),
+        webContent = document.querySelector('.web'),
+        adContent = document.querySelector('.ad');
 
-        tabAdBtn.addEventListener('click', () => {
-            tabAdBtn.classList.add('header__menu-ad--active');
-            tabWebBtn.classList.remove('header__menu-web--active');
-        });
+    function showAdTab(e) {
+        e.preventDefault();
 
-        tabWebBtn.addEventListener('click', () => {
-            tabAdBtn.classList.remove('header__menu-ad--active');
-            tabWebBtn.classList.add('header__menu-web--active');
-        });
+        tabAdBtn.classList.add('header__menu-ad--active');
+        tabWebBtn.classList.remove('header__menu-web--active');
+        webContent.classList.add('hidden');
+        adContent.classList.remove('hidden');
+    }
+
+    function showWebTab(e) {
+        e.preventDefault();
+
+        tabAdBtn.classList.remove('header__menu-ad--active');
+        tabWebBtn.classList.add('header__menu-web--active');
+        webContent.classList.remove('hidden');
+        adContent.classList.add('hidden');
+    }
+
+    tabAdBtn.addEventListener('click', showAdTab);
+
+    tabWebBtn.addEventListener('click', showWebTab);
+
+    promoAdBtn.addEventListener('click', showAdTab);
+
+    promoWebBtn.addEventListener('click', showWebTab);
+
 })
